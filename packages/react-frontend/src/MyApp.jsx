f461eb0ec7a7eb1c2b38102f1237b46cf0322636
EvaGut <evag8928@gmail.com>
2026-04-03 11:53:33 -0700

    completed all tasks

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-  ]);
+  const [characters, setCharacters] = useState([]);
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
-    setCharacters(updated);
-  }
+    setCharacters(updated);}
+
+   function updateList(person) {
+    setCharacters([...characters, person]);
+    }
+  
 
 
 	
@@ -34,7 +22,8 @@ function MyApp() {
       <Table
 	  characterData = {characters}
 	  removeCharacter={removeOneCharacter}
-	  />
+      />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
