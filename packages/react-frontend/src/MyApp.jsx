21fc0451f3a5dab7897198a40fdc64c6cb0d2eef
EvaGut <evag8928@gmail.com>
2026-04-14 16:10:58 -0700

    Task 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -11,16 +11,31 @@ function MyApp() {
     });
     setCharacters(updated);}
 
-   function updateList(person) {
-    setCharacters([...characters, person]);
-    }
+   function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
   
    function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
 }
+    function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
 
-    useEffect(() => {
+    return promise;
+  }
+   
+   useEffect(() => {
      fetchUsers()
 	  .then((res) => res.json())
 	  .then((json) => setCharacters(json["users_list"]))
