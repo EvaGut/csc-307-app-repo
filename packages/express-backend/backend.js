3b44e29c23c48e163e4a67a2a92f1c3ea8177ea5
EvaGut <evag8928@gmail.com>
2026-04-30 21:15:12 -0700

    Task Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,46 +1,78 @@
 import express from "express";
 import cors from "cors";
+import userservices from "./Models/user-services.js"
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
+
 
 app.get("/", (req, res) => {
-  res.send(users);
+  res.send("Hello World :) ");
 });
 
-const findUserByName = (name) => {
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+    if(name != undefined && job != undefined)
+    {
+     userservices.findUserByName_Job(name,job)
+     .then(result => res.send({users_list :result}))
+     .catch(error => res.status(500).send({error: error.message}));
+    }
+    else if(name)
+    {
+      userservices.findUserByName(name)
+     .then(result => res.send({users_list :result}))
+     .catch(error => res.status(500).send({error: error.message}));
+    }
+    else if(job)
+    {
+     userservices.findUserByJob(job)
+     .then(result => res.send({users_list :result}))
+     .catch(error => res.status(500).send({error: error.message}));
+    }
+    else 
+    {
+    userservices.getUsers()
+    .then(result => res.send({users_list :result}))
+    .catch(error => res.status(500).send({error: error.message}));
+
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  userservices.findUserById(id)
+  .then(result =>{if (result === null) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }})
+  .catch(error => res.status(500).send({error: error.message}));
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+    userservices.deleteUserById(id)
+  .then(result =>{if (result === null) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(204).send();
+  }})
+  .catch(error => res.status(500).send({error: error.message}));
+
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  userservices.addUser(userToAdd)
+  .then(result => res.status(201).send(result))
+  .catch(error => res.status(400).send({error: error.message}));
+});
+/*const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
@@ -113,7 +145,7 @@ app.post("/users", (req, res) => {
   addUser(userToAdd);
   res.status(201).send(userToAdd);
 });
-
+*/
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
